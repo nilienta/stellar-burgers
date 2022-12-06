b7111e9cf45a1aeaee2b7163435815cd062602a3
nilienta <97403514+nilienta@users.noreply.github.com>
2022-12-06 12:10:51 +0400

    Add wsReducer and wsReducerToken in Reducer

--- src/services/reducers/index.ts
+++ src/services/reducers/index.ts
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { appReducer } from './app';
 import { authReducer } from './auth';
+import { wsReducer } from './web-socket';
+import { wsReducerToken } from './web-socket-token';
 
 export const rootReducer = combineReducers({
   app: appReducer,
   auth: authReducer,
+  ws: wsReducer,
+  wsToken: wsReducerToken,
 });
