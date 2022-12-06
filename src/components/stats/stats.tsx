cc0742ee3dec3aee9f6c1893c016edd6cf1a1479
nilienta <97403514+nilienta@users.noreply.github.com>
2022-12-06 11:59:21 +0400

    create Stats component

--- src/components/stats/stats.tsx
+++ src/components/stats/stats.tsx
@@ -1,68 +1,50 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import styles from './stats.module.css';
 import clsx from 'clsx';
+import { TCurrentOrder } from '../../utils/types';
+import { useAppSelector } from '../../utils/types';
 
 const Stats: FC = () => {
+  const { totalToday, total, orders } = useAppSelector((state) => state.ws);
+  const completedToday = totalToday;
+  const completedAllTime = total;
+
+  const { done, inWork } = useMemo(
+    () => ({
+      done: orders.filter((item: TCurrentOrder) => item.status === 'done'),
+      inWork: orders.filter((item: TCurrentOrder) => item.status !== 'done'),
+    }),
+    [orders]
+  );
+
   return (
     <article className={styles.container}>
       <section className={styles['order-number']}>
         <section className={styles.column}>
           <p className="text text_type_main-medium mb-6">Готовы:</p>
           <div className={styles['list-number']}>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              034533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              034533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              034533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              034533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              034553
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              036533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              564533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              034533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              034553
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              036533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              564533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              034533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              034553
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              036533
-            </p>
-            <p className="text text_type_digits-default text_color_success mb-2">
-              564533
-            </p>
+            {done.map((item: TCurrentOrder, index: number) => {
+              return (
+                <p
+                  className="text text_type_digits-default text_color_success mb-2"
+                  key={index}
+                >
+                  {item.number}
+                </p>
+              );
+            })}
           </div>
         </section>
         <section className={styles.column}>
           <p className="text text_type_main-medium mb-6">В работе:</p>
           <div className={styles['list-number']}>
-            <p className="text text_type_digits-default mb-2">034533</p>
-            <p className="text text_type_digits-default mb-2">034553</p>
-            <p className="text text_type_digits-default mb-2">036533</p>
-            <p className="text text_type_digits-default mb-2">564533</p>
+            {inWork.map((item: TCurrentOrder) => {
+              return (
+                <p className="text text_type_digits-default mb-2">
+                  {item.number}
+                </p>
+              );
+            })}
           </div>
         </section>
       </section>
@@ -71,13 +53,13 @@ const Stats: FC = () => {
           Выполнено за все время:
         </p>
         <p className={clsx('text text_type_digits-large', styles.number)}>
-          28 752
+          {completedAllTime}
         </p>
       </section>
       <section className={styles.row}>
         <p className="text text_type_main-medium">Выполнено за сегодня:</p>
         <p className={clsx('text text_type_digits-large', styles.number)}>
-          138
+          {completedToday}
         </p>
       </section>
     </article>
